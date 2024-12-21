39c3202d1437ec4c74cb33e2e8eb026f2af19075
unesssss <main.michelleeunice.opena@cvsu.edu.ph>
2024-12-21 17:56:12 +0800

    import undrawtodolist in icons.js

--- constants/icons.js
+++ constants/icons.js
@@ -1,9 +1,11 @@
 import add from "../assets/icons/add.png";
 import prof from "../assets/icons/prof.png";
 import todo from "../assets/icons/todo.png";
+import undrawToDoList from "../assets/images/undraw_to-do-list_dzdz.png";
 
 export default {
   add,
   prof,
   todo,
+  undrawToDoList
 };
