83ffe103ed22ec9030ba65e1b6fd695e5f4e9e59
unesssss <main.michelleeunice.opena@cvsu.edu.ph>
2024-12-21 21:08:08 +0800

    add illustration image

--- constants/icons.js
+++ constants/icons.js
@@ -2,10 +2,14 @@ import add from "../assets/icons/add.png";
 import prof from "../assets/icons/prof.png";
 import todo from "../assets/icons/todo.png";
 import undrawToDoList from "../assets/images/undraw_to-do-list_dzdz.png";
+import back from "../assets/images/back.png";
+import task from "../assets/images/task.png";
 
 export default {
   add,
   prof,
   todo,
-  undrawToDoList
+  undrawToDoList,
+  back,
+  task
 };
